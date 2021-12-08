2b4fae8957eac926fa84207d6250081d3511a5d5
ItaiCuker <itano1234@gmail.com>
2021-12-08 19:58:24 +0200

    updated colors and UI

--- app/src/main/java/com/itaicuker/unimot/LoginActivity.java
+++ app/src/main/java/com/itaicuker/unimot/LoginActivity.java
@@ -2,13 +2,17 @@ package com.itaicuker.unimot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class loginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
