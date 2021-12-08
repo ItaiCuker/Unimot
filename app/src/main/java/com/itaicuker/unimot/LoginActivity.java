1d5976eedb29854f4e49c26533533b7d170d0508
ItaiCuker <itano1234@gmail.com>
2021-12-08 21:59:34 +0200

    added launcher icon, fixed again login and register layouts

--- app/src/main/java/com/itaicuker/unimot/LoginActivity.java
+++ app/src/main/java/com/itaicuker/unimot/LoginActivity.java
@@ -14,5 +14,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        findViewById(R.id.btnSignIn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
     }
 }
