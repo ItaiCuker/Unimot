86bb4f9993243b23127fbba2d9fdd9c831b17fd7
itai <itano1234@gmail.com>
2021-12-14 14:47:33 +0200

    did some stuff |:

---
+++ app/src/main/java/com/itaicuker/unimot/ui/DeviceCard.java
@@ -0,0 +1,12 @@
+package com.itaicuker.unimot.ui;
+
+import android.content.Context;
+
+import com.google.android.material.card.MaterialCardView;
+
+public class DeviceCard extends MaterialCardView
+{
+    public DeviceCard(Context context) {
+        super(context);
+    }
+}
