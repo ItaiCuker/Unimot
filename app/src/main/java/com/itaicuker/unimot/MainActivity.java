ed4aa9a58043e685a39cffa2d405d1fab5eb1fc7
talmid <a@gmail.com>
2022-02-24 12:38:14 +0200

    nothing working yayks

--- app/src/main/java/com/itaicuker/unimot/MainActivity.java
+++ app/src/main/java/com/itaicuker/unimot/MainActivity.java
@@ -1,14 +1,21 @@
 package com.itaicuker.unimot;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationUI.setupWithNavController(toolbar, Navigation.findNavController(this, R.id.nav_host_fragment));
     }
 }
