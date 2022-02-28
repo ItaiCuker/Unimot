fa61b50a06e78e47f736e8cec850e5afcaf31461
itai <itano1234@gmail.com>
2022-02-28 12:19:50 +0200

    aaa

--- app/src/main/java/com/itaicuker/unimot/MainActivity.java
+++ app/src/main/java/com/itaicuker/unimot/MainActivity.java
@@ -1,21 +1,32 @@
 package com.itaicuker.unimot;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
+    private NavHostFragment navHostFragment;
+    private Toolbar toolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        NavigationUI.setupWithNavController(toolbar, Navigation.findNavController(this, R.id.nav_host_fragment));
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        toolbar = findViewById(R.id.toolbar);
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
     }
 }
